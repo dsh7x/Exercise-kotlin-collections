d2187aa0f059e5215f4287fb41af5ecc732f92d8
dsh7x <dshx7x@gmail.com>
2025-02-13 18:50:32 +0300

    Added MaximumAndMinimum

---
+++ collections/MaximumAndMinimum.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val elementsList = listOf(2.5, 7.1, 3.8, 9.2, 5.6)
+    println("Largest number: ${elementsList.maxOrNull()}")
+    println("Smallest number: ${elementsList.minOrNull()}")
+}
