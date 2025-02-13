5c434fa2bf5b7d37ee11dc88c7b53e78270c1307
dsh7x <dshx7x@gmail.com>
2025-02-13 18:22:16 +0300

    Added mutableMap file

---
+++ collections/mutableMap.kt
@@ -0,0 +1,10 @@
+fun main (){
+    val countryCapitals = mutableMapOf("USA" to "Washington",
+        "France" to "Paris",
+        "Germany" to "Berlin")
+    countryCapitals["Canada"] = "Ottawa"
+    println("Countries: ${countryCapitals.keys} ")
+    println("Capitals: ${countryCapitals.values} ")
+    countryCapitals.remove("Germany")
+    println("Final Map: ${countryCapitals} ")
+}
